54eb68d412f9389c13a3c1738590afbaa812e1e9
Dollat <dollat4real@gmail.com>
2022-07-01 12:56:30 -0700

    task 1

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -4,7 +4,7 @@
 #include <stdio.h>
 
 /**
- * main - a function that print english alphabets in lower case.
+ * main - a function that print english alphabets in lower case and upper case.
  * ------
  *  Return: zero
  */
@@ -12,13 +12,18 @@ int main(void)
 
 {
 	char alpha = 'a';
+	char ALPHA = 'A';
 
 	while (alpha <= 'z')
 	{
 		putchar(alpha);
 		alpha++;
 	}
+	while (ALPHA <= 'Z')
+	{
+		putchar('ALPHA');
+		ALPHA++;
+	}
 	putchar('\n');
 	return (0);
-
 }
