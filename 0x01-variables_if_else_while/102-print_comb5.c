13add0b9ec7c1c1df3d82ff1d3258b0bf36049ca
Dollat <dollat4real@gmail.com>
2022-07-01 22:20:01 -0700

    task 9

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+
+
+#include <stdio.h>
+
+/**
+ *main - a function that print hexadecimal numbers.
+ * ------
+ *  Return: zero
+ */
+int main(void)
+
+{
+	int digit1 = 0;
+	int digit2 = 0;
+	int count = 0;
+
+	while (digit1 <= 9)
+	{
+		putchar(digit1);
+		digit1++;
+		while(digit3 <= 9)
+		{
+			putchar(digit2);
+			digit2++;
+			count++;
+		}
+	}
+	if (count < 100)
+	{
+		putchar(',');
+		putchar(' ');
+	}
+	else
+		putchar('\n');
+	return (0);
+
+}
+
