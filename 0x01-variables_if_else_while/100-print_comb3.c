33582a75b8c77cfad044a4ec2ed139d3b84993f8
Dollat <dollat4real@gmail.com>
2022-07-01 22:52:41 -0700

    task 9u

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+
+
+#include <stdio.h>
+
+/**
+ *main - a function that print hexadecimal numbers.
+ * ------
+ *  Return: zero
+ */
+int main(void)
+
+{
+	int i, j = 0;
+	int count = 0;
+	
+	for(i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			if (i < j)
+			{
+				putchar('0' + i);
+				putchar('0' + j);
+			}
+		}
+	}
+}
