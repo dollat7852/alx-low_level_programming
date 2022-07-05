6d8b380bc49a6fc1f211f5f9c55c16a57c30a2de
Dollat <dollat4real@gmail.com>
2022-07-05 16:40:11 -0700

    initial commit

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,18 +12,15 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{	_putchar('+');
-		_putchar('\n');
 		return (1);
 	}
 	else if (n == 0)
 	{	_putchar('0' + 0);
-		_putchar('\n');
 		return (0);
 	}
 	else
 	{
 		_putchar('-');
-		_putchar('\n');
 		return (-1);
 	}
 }
