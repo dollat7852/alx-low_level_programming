5ec13a9f855236c3ba41cd2785cf9e4e21931e77
Dollat <dollat4real@gmail.com>
2022-07-05 17:23:46 -0700

    initial commit

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -22,7 +22,7 @@ void jack_bauer(void)
 					{
 						_putchar('0' + 0);
 					}
-					_putchar(j + '0');
+					_putchar((j) + '0');
 					_putchar(':');
 					_putchar('0' + y);
 					_putchar('0' + z);
