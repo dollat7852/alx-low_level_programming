4c6cbd95f3f92d769f4526a1ac4a5b140978eb47
Dollat <dollat4real@gmail.com>
2022-07-07 14:29:32 -0700

    task 5

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -9,6 +9,8 @@ void print_square(int size)
 {
 	int i, j;
 
+	if (size > 0)
+	{
 	for (i = 0; i < size; i++)
 	{
 		for (j = 0; j < size; j++)
@@ -17,4 +19,7 @@ void print_square(int size)
 		}
 		_putchar('\n');
 	}
+	}
+	else
+		_putchar('\n');
 }
