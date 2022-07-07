3bb81233a3507220562ab764313eee113ec6f8f2
Dollat <dollat4real@gmail.com>
2022-07-07 14:45:31 -0700

    task 5

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -9,6 +9,8 @@ void print_triangle(int size)
 {
 	int i, j;
 
+	if (size > 0)
+	{
 	for (i = 0; i < size; i++)
 	{
 
@@ -21,4 +23,7 @@ void print_triangle(int size)
 		}
 		_putchar('\n');
 	}
+	}
+	else
+		_putchar('\n');
 }
