30d1b3aaff961643c43ba2448142070325c54560
Dollat <dollat4real@gmail.com>
2022-07-07 14:02:26 -0700

    task 5

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -6,10 +6,15 @@
  */
 void print_diagonal(int n)
 {
-	int i;
+	int i, j;
 
 	for (i = 0; i < n; i++)
 	{
+		while (j < i)
+		{
+			_putchar(' ');
+			j++;
+		}
 		_putchar('\\');
 	}
 		_putchar('\n');
