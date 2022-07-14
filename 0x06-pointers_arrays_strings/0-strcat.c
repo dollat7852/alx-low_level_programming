47ddf9d282f10ecbbfae7753a0813d96b4914d7e
Dollat <dollat4real@gmail.com>
2022-07-14 07:47:27 -0700

    task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcat - join two strings together
+ *
+ * @dest: arg 1
+ * @src: arg 2
+ *
+ * Return: a pointer to the joined string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	strcat(dest, src);
+
+	return (*dest);
+}
