a131e04bbf042390dc3626f226b646ab5844efdd
Dollat <dollat4real@gmail.com>
2022-07-14 09:05:56 -0700

    task 0

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcmp - compare two strings together
+ *
+ * @dest: arg 1
+ * @src: arg 2
+ *
+ * Return: a pointer to the joined string
+ */
+
+int _strcmp(char *dest, char *src)
+{
+	return (strcmp(dest, src));
+}
