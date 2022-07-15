4d8df491949d8314555eb2db4a819ea192607836
Dollat <dollat4real@gmail.com>
2022-07-14 22:40:04 -0700

    task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,6 +11,7 @@
 cap_string(char *s)
 {
 	int len = strlen(s) - 1;
+	int i;
 
 	if (s[0] >= 'a' && s[0] <= 'z')
 		s[0] = s[0] - 32;
@@ -18,8 +19,10 @@ cap_string(char *s)
 	{
 		if (s[i] == ',' || s[i] == '.' || s[i] == ' ' || s[i] == ';' || s[i] == '(' || s[i] == ')' || s[i] == '{' || s[i] == '}' || s[i] == '!' || s[i] == '?')
 		{
-			if (s[i] >= 'a' && s[i] <= 'z')
-				s[i] = s[i] - 32;
+			if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+				s[i + 1] = s[i + 1] - 32;
+			else if (s[i + 2] >= 'a' && s[i +2] <= 'z')
+				s[i + 2] = s[i + 2] - 32;
 		}
 	}
 	return (0);
