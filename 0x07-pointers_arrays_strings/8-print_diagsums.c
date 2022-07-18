0da51c3968c9b452aca61849284ec5cb53f06c1f
Dollat <dollat4real@gmail.com>
2022-07-18 15:41:48 -0700

    tasks 8

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -6,7 +6,6 @@
  * print_diagsums - print an size by size matrix
  * @a: number of rows
  * @size: sie of a square matrix
- * Return: void
  */
 
 void print_diagsums(int *a, int size)
@@ -14,10 +13,15 @@ void print_diagsums(int *a, int size)
 	int i;
 	int j;
 	int sum = 0;
+	int sum2 = 0;
 
 	for (i = 0; i < size * sie; i = i + size + 1)
 	{
 		sum += a[i];
 	}
-	printf("%d", sum);
+	for (j = size - 1; j < size * size - 1; j = j + size - 1)
+	{
+		sum2 += a[j];
+	}
+	printf("%d, %d\n", sum, sum2);
 }
