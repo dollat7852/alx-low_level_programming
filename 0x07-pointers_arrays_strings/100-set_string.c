b13b6d937f58663d9981106edeacf28554d85c10
Dollat <dollat4real@gmail.com>
2022-07-18 15:52:21 -0700

    task 9

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * set_string - set pointer value to string
+ * @s: char 1
+ * @to: var  2
+ */
+
+void set_string(char **s, char *to)
+{
+	char *tmp;
+
+	*tmp = *to;
+	*to = *tmp;
+}
