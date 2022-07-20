1d602114be7505112bb6cdb7e4930af6e740b42f
Dollat <dollat4real@gmail.com>
2022-07-20 02:01:19 -0700

    nitial commit

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts_recursion - write to stdout recursively
+ * @s: string to be written
+ *
+ */
+
+void _puts_recursion(char *s)
+{
+	return(puts(s));
+}
