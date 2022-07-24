f8a887143a2ce00db56c8ec7b192d9063edd74e4
Dollat <dollat4real@gmail.com>
2022-07-24 07:23:32 -0700

    addded stdlib dependency

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -14,9 +14,9 @@ int main(int argc, char *argv[])
 	int i;
 	int mul = 1;
 
-	if  (argc > 1)
+	if  (argc > 2)
 	{
-		for (i = 0; i < argc; i++)
+		for (i = 1; i < argc; i++)
 			mul *= atoi(argv[i]);
 	printf("%d\n", mul);
 	return (0);
