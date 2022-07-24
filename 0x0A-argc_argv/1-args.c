35a39e8e93b749a443184d50082fcca5a123535c
Dollat <dollat4real@gmail.com>
2022-07-24 06:23:12 -0700

    t1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - a function
+ * @argc: arg counter
+ * @argv: arg vector
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
