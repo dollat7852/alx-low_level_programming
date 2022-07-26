2a37539c771ec209ba0b641e5f2696aa48a79249
Dollat <dollat4real@gmail.com>
2022-07-26 04:05:20 -0700

    task 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -20,7 +20,7 @@ int **alloc_grid(int width, int height)
 	if (width < 1 || height < 1 || output == 0)
 		return (NULL);
 
-	for (i = 0; i < width; i++)
+	for (i = 0; i < height; i++)
 	{
 		output[i] = malloc(width * sizeof(**output));
 		if (output[i] == 0)
@@ -30,7 +30,7 @@ int **alloc_grid(int width, int height)
 			free(output);
 			return (NULL);
 		}
-		for (j = 0; j < height; j++)
+		for (j = 0; j < width; j++)
 		{
 			output[i][j] = 0;
 		}
