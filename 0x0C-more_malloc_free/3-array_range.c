e4824404cfc5938718f9c011c6d69b9f65a2d2e5
Dollat <dollat4real@gmail.com>
2022-07-28 14:46:47 -0700

    fourth is the charm

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -15,9 +15,11 @@ int *array_range(int min, int max)
 {
 	int range, i = 0,  *ptr;
 
+	if (min > max)
+		return (NULL);
 	range  = max - min;
 	ptr = malloc((range + 1) * sizeof(*ptr));
-	if (min > max || ptr == NULL)
+	if (ptr == NULL)
 		return (NULL);
 	for (range = min; range <= max; range++)
 		ptr[i++] = range;
