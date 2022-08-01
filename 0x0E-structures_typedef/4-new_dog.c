13a2bce8f0e91e0aa21f54d058d1872ffdfdcd39
Dollat <dollat4real@gmail.com>
2022-08-01 12:16:10 -0700

    fix check 6 and 8

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -20,8 +20,12 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	if (new_dog == NULL)
 		return (NULL);
+	if (name == NULL)
+		return (NULL);
 	new_dog->name = name;
 	new_dog->age = age;
+	if (owner == NULL)
+		return (NULL);
 	new_dog->owner = owner;
 	return (new_dog);
 }
