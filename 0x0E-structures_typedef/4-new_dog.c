64cab3c043bd374270dca517a30b2aed0f1ce425
Dollat <dollat4real@gmail.com>
2022-08-01 12:35:20 -0700

    task 4-=

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -32,6 +32,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	new_dog->owner = malloc(strlen(owner) * sizeof(char) + 1);
 	if (new_dog->owner == NULL)
 	{
+		free(new_dog->name);
 		free(new_dog);
 		return (NULL);
 	}
