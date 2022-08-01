c1bbaf9912d5eee9a6fc54a98cb5613a31009e60
Dollat <dollat4real@gmail.com>
2022-08-01 07:25:26 -0700

    fixed unused variable error

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,6 +13,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
 
+	(void) age; /*unused variable */
+
 	/* create memory space for new_dog*/
 	new_dog = malloc(sizeof(dog_t));
 
