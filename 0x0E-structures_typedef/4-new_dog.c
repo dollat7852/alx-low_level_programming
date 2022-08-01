c4009dfa692b723a328696a26204d11fa6fafb22
Dollat <dollat4real@gmail.com>
2022-08-01 12:23:17 -0700

    task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "dog.h"
 #include <stdlib.h>
+#include <string.h>
 /**
  * new_dog - a new dog function that stores only dog name and owner.
  * @name: pointer
@@ -12,6 +13,7 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
+	int len;
 
 /*	(void) age; unused variable */
 
@@ -20,12 +22,16 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	if (new_dog == NULL)
 		return (NULL);
-	if (name == NULL)
+	len = strlen(name);
+	new_dog->name = malloc(len * sizeof(char) + 1);
+	if (new_dog->name == NULL)
+		return (NULL);
+	len = strlen(owner);
+	new_dog->owner = malloc(len * sizeof(char) + 1);
+	if (new_dog->owner == NULL)
 		return (NULL);
 	new_dog->name = name;
 	new_dog->age = age;
-	if (owner == NULL)
-		return (NULL);
 	new_dog->owner = owner;
 	return (new_dog);
 }
