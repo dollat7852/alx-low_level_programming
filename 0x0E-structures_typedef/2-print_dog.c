370b48dac829047b7fb8ce2f82575f378bf21e42
Dollat <dollat4real@gmail.com>
2022-08-01 06:35:23 -0700

    print_dog

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - print members of struct dog
+ * @d: pointer to dog
+ */
+
+void print_dog(struct dog *d)
+{
+	printf("Name: %s\n", d->name);
+	printf("Age: %.6f\n", d->age);
+	printf("Owner: %s\n", d->owner);
+}
