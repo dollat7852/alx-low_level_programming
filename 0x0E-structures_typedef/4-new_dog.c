fd1165a827f6d84b14de034df4760957bea58448
Dollat <dollat4real@gmail.com>
2022-08-01 07:28:12 -0700

    ---

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,7 +13,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
 
-	(void) age; /*unused variable */
+/*	(void) age; unused variable */
 
 	/* create memory space for new_dog*/
 	new_dog = malloc(sizeof(dog_t));
@@ -21,6 +21,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (new_dog == NULL)
 		return (NULL);
 	new_dog->name = name;
+	new_dog->age = age;
 	new_dog->owner = owner;
 	return (new_dog);
 }
