8f0d39ee24f06bfeb4a758f8555d208552ec6768
Dollat <dollat4real@gmail.com>
2022-08-01 06:43:05 -0700

    addedd the conditions

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,7 +8,16 @@
 
 void print_dog(struct dog *d)
 {
+	if (d == NULL)
+		return;
+	if (d->name == NULL)
+		d->name = "(nil)";
+	if (d->owner == NULL)
+		d->owner = "(nil)"
 	printf("Name: %s\n", d->name);
-	printf("Age: %.6f\n", d->age);
+	if (d->age == NULL)
+		printf("Age: %s\n", "(nil)");
+	else
+		printf("Age: %.6f\n", d->age);
 	printf("Owner: %s\n", d->owner);
 }
