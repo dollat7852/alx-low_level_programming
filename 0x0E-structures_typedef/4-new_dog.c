7b1ee2e1378bd7c389cc128c38f963db007d03c4
Dollat <dollat4real@gmail.com>
2022-08-01 16:35:12 -0700

    task 4 again

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,43 +1,43 @@
-#include <stdio.h>
 #include <string.h>
 #include "dog.h"
+#include <stdio.h>
 #include <stdlib.h>
+
 /**
- * new_dog - a new dog function that stores only dog name and owner.
- * @name: pointer
- * @age: float
- * @owner: pointer
- * Return: pointer to the new dog.
+ * new_dog - create new data structure for dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: dog owner
+ *
+ * Return: Pointer to new dog
  */
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
-	int len;
+	int len_name, len_owner;
 
-/*	(void) age; unused variable */
-
-	/* create memory space for new_dog*/
 	new_dog = malloc(sizeof(dog_t));
-
 	if (new_dog == NULL)
 		return (NULL);
-	len = strlen(name);
-	new_dog->name = malloc(len * sizeof(char) + 1);
+
+	len_name = strlen(name);
+	new_dog->name = malloc(sizeof(char) * len_name + 1);
 	if (new_dog->name == NULL)
 	{
 		free(new_dog);
 		return (NULL);
 	}
-	new_dog->name = name;
-	new_dog->owner = malloc(strlen(owner) * sizeof(char) + 1);
+	new_dog->name = strcpy(new_dog->name, name);
+	len_owner = strlen(owner);
+	new_dog->owner = malloc(sizeof(char) * len_owner + 1);
 	if (new_dog->owner == NULL)
 	{
 		free(new_dog->name);
 		free(new_dog);
 		return (NULL);
 	}
+	new_dog->owner = strcpy(new_dog->owner, owner);
 	new_dog->age = age;
-	new_dog->owner = owner;
 	return (new_dog);
 }
