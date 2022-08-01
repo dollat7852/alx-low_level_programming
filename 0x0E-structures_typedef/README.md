9e5def3a97d154abc85411ac0e8217ac9d6f2aee
Dollat <dollat4real@gmail.com>
2022-08-01 05:43:50 -0700

    structs

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+structure and typedef in c
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+
+/**
+ * struct dog - a dog description
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: dog owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
