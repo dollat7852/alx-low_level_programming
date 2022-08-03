6900982989995ccb4e133f8885b2a0e296f905e2
Dollat <dollat4real@gmail.com>
2022-08-03 16:42:22 -0700

    task 3

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * int_index - return the index returned by cmp
+ * @cmp: function pointer
+ * @size: size of an array
+ * @array: array of int
+ * Return: int -1 if size <=0 index otherwise
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i =0;
+
+	if (array == NULL || cmp == NULL || size < 1)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+				return (i);
+	}
+	return (-1);
+}
