ecb838e5ca0891869a9cb8f32def2bdf6bc8fab8
Dollat <dollat4real@gmail.com>
2022-08-03 15:57:47 -0700

    t1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -15,6 +15,6 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 
 	if (size < 1 || action == NULL)
 		return;
-	for (i = 0; i <= size; i++)
+	for (i = 0; i < size; i++)
 		action(array[i]);
 }
