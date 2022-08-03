d2a19adc905a1a474665d8b04ea619bd50856130
Dollat <dollat4real@gmail.com>
2022-08-03 16:06:36 -0700

    fixed comparision between int and size_t

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -2,8 +2,8 @@
 #include "function_pointers.h"
 
 /**
- * array_iterator - function that takes an array of functions 
- * and execute the function at array[size] on array
+ * array_iterator - function that takes an array of functions
+ * * and execute the function at array[size] on array
  * @array: int pointer
  * @size: selector
  * @action: function pointer
@@ -13,7 +13,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i = 0;
 
-	if (size < 1 || action == NULL)
+	if (array == NULL || action == NULL)
 		return;
 	for (i = 0; i < size; i++)
 		action(array[i]);
