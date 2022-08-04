3fb243a6fa17c41b1ddf1aa072c6a98bd5c3039b
Dollat <dollat4real@gmail.com>
2022-08-04 16:14:33 -0700

    fixed

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -14,8 +14,6 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list ap;
 	unsigned int i = 0;
 
-	if (n == 0)
-		return;
 	va_start(ap, n);
 		for (i = 0; i < n ; i++)
 		{
