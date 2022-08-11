27b5a4fdd6a348fdc69b73636d25601eeebfb479
Dollat <dollat4real@gmail.com>
2022-08-11 13:40:01 -0700

    initial commit

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * print_list - function to print nodes of a list
+ * @h: pointer to the head of the list
+ * Return: size_t
+ */
+
+size_t print_list(const list_t *h)
+{
+	int count = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		count++;
+		h = h->next;
+		}
+		return (count);
+}
