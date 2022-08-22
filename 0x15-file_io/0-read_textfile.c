20a91c9e7ac2a2763d32711e60eccb43eef9ab4e
Dollat <dollat4real@gmail.com>
2022-08-22 14:29:14 -0700

    fix compilation error on task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -25,7 +25,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 
 	a = open(filename, O_RDONLY);
-	b = read(o, buffer, letters);
+	b = read(a, buffer, letters);
 	c = write(STDOUT_FILENO, buffer, b);
 
 	if (a == -1 || b == -1 || c == -1 || c != b)
