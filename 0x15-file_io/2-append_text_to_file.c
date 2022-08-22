f79f5628463bb12cbf751132499d1f785ee09603
Dollat <dollat4real@gmail.com>
2022-08-22 14:18:30 -0700

    fix spelling mistake

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -19,7 +19,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-		len = strlen(text_context);
+		len = strlen(text_content);
 	/*
 		for (len = 0; text_content[len];)
 			len++;
